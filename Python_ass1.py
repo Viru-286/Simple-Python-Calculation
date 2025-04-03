1afb1d5ff92370dd3b013ddd009719303d6aa937
Viru-286 <jangirviren380@gmail.com>
2025-04-03 23:55:55 +0530

    Add files via upload

---
+++ Python_ass1.py
@@ -0,0 +1,24 @@
+#Task 1 SIMPLE CALCULAtio:
+
+a = input('Enter you First number:')
+b = input('Enter you Second number:')
+a = float(a)
+b = float(b)
+
+
+Add = a + b
+Sub = a - b
+Mult = a * b
+Div = a/b
+
+print('Addition:',Add)
+print('Subtraction:',Sub)
+print('Multiplication:',Mult)
+print('Division:',Div)
+
+#Task 2: Creating personalized Greeting:
+
+c = input('Enter your First name:')
+d = input('Enter your last name:')
+
+print('Hello Mr '+ c + ' ' + d+'!,Welcome to Python program.')
